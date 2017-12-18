6702e749a3cc04b334b432fa2fa3fd9bb8b4af0d
moochannel <moochannel@gmail.com>
2017-12-18 16:18:03 +0900

    bootstrapのダウンロードをDropboxに設置されたミラーに変更します

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,10 +16,14 @@ _download_from_gdrive() {
 	curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${download_code}&id=$1" -o $2
 }
 
+_download_from_dropbox() {
+	curl -Ls "https://www.dropbox.com/s/wz8sg14ujmx1dnm/xpcoin-bootstrap-peers.zip?dl=0" -o $1
+}
+
 _deploy_bootstrap() {
 	rm -fr $2/*
 	unzip -d $2 $1
-	for CONTENT_FILE in $(find ${2} -type f -and -not -name readme.txt -and -not -name xpcoin-startkit_v1.0.bat)
+	for CONTENT_FILE in $(find ${2} -type f -and -not -name readme.txt -and -not -name xpcoin-startkit_v1.0.bat -and -not -name wallet.dat)
 	do
 		mv ${CONTENT_FILE} ${XPD_DATA_DIR}/$(echo ${CONTENT_FILE} | sed -r 's/^([^\/]*\/){2}//')
 	done
@@ -28,7 +32,7 @@ _deploy_bootstrap() {
 }
 
 if [ ! -d ${XPD_DATA_DIR}/database ]; then
-	_download_from_gdrive ${FILEID} bootstrap-latest.zip
+	_download_from_dropbox bootstrap-latest.zip
 	_deploy_bootstrap bootstrap-latest.zip ziptemp
 fi
 
